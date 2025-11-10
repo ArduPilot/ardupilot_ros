d7c72dba4d5f2b9e97c5e8441d9136e57c825732
Stephen Dade <stephen_dade@hotmail.com>
2025-11-10 20:31:36 +1100

    nav2: Performance improvements

--- ardupilot_cartographer/config/cartographer.lua
+++ ardupilot_cartographer/config/cartographer.lua
@@ -24,11 +24,12 @@ options = {
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 0.5,
+  imu_sampling_ratio = 0.1,  -- Reduced from 0.5 to lower IMU influence
   landmarks_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
+MAP_BUILDER.num_background_threads = 2
 
 TRAJECTORY_BUILDER_2D.min_range = 0.05
 TRAJECTORY_BUILDER_2D.max_range = 30
@@ -36,6 +37,8 @@ TRAJECTORY_BUILDER_2D.missing_data_ray_length = 8.5
 TRAJECTORY_BUILDER_2D.use_imu_data = true
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 0.2
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 5
+-- Reduce IMU influence in scan matching
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 20
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1.
@@ -51,5 +54,22 @@ POSE_GRAPH.constraint_builder.min_score = 0.65
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.65
 POSE_GRAPH.optimization_problem.huber_scale = 1e2
 POSE_GRAPH.optimize_every_n_nodes = 30
+
+-- Additional parameters to reduce jumping
+POSE_GRAPH.optimization_problem.odometry_translation_weight = 1e5  -- Higher = trust odometry more
+POSE_GRAPH.optimization_problem.odometry_rotation_weight = 1e5
+POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1.1e4
+POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 1e5
+POSE_GRAPH.constraint_builder.max_constraint_distance = 15.  -- Limit loop closure distance
+POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 1e5
+POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 1e5
+
+-- Reduce the search window for loop closures
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 7.
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(30.)
+
+-- Lower IMU weights to reduce influence on pose estimation
+POSE_GRAPH.optimization_problem.acceleration_weight = 1e2
+POSE_GRAPH.optimization_problem.rotation_weight = 1e2
 
 return options
