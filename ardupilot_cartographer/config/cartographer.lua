dc7f7c49d477774ae83b858c2a473bed7d301c37
Stephen Dade <stephen_dade@hotmail.com>
2025-12-02 19:28:25 +1100

    ardupilot_cartographer: Better map stability for cartographer

--- ardupilot_cartographer/config/cartographer.lua
+++ ardupilot_cartographer/config/cartographer.lua
@@ -18,32 +18,33 @@ options = {
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
-  submap_publish_period_sec = 0.3,
-  pose_publish_period_sec = 5e-3,
-  trajectory_publish_period_sec = 30e-3,
+  submap_publish_period_sec = 0.5,
+  pose_publish_period_sec = 10e-3,
+  trajectory_publish_period_sec = 50e-3,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 0.1,  -- Reduced from 0.5 to lower IMU influence
+  imu_sampling_ratio = 1.,
   landmarks_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
-MAP_BUILDER.num_background_threads = 2
+MAP_BUILDER.num_background_threads = 1
 
-TRAJECTORY_BUILDER_2D.min_range = 0.05
+TRAJECTORY_BUILDER_2D.min_range = 0.27
 TRAJECTORY_BUILDER_2D.max_range = 30
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 8.5
 TRAJECTORY_BUILDER_2D.use_imu_data = true
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 0.2
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 5
--- Reduce IMU influence in scan matching
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 20
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 10
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1.
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 10
 TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.2)
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.0
+TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 0.2
 -- for current lidar only 1 is good value
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 
@@ -55,21 +56,13 @@ POSE_GRAPH.constraint_builder.global_localization_min_score = 0.65
 POSE_GRAPH.optimization_problem.huber_scale = 1e2
 POSE_GRAPH.optimize_every_n_nodes = 30
 
--- Additional parameters to reduce jumping
-POSE_GRAPH.optimization_problem.odometry_translation_weight = 1e5  -- Higher = trust odometry more
-POSE_GRAPH.optimization_problem.odometry_rotation_weight = 1e5
-POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1.1e4
-POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 1e5
-POSE_GRAPH.constraint_builder.max_constraint_distance = 15.  -- Limit loop closure distance
-POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 1e5
-POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 1e5
+-- Reduce IMU influence on pose estimation
+POSE_GRAPH.optimization_problem.acceleration_weight = 1e1
+POSE_GRAPH.optimization_problem.rotation_weight = 1e1
 
--- Reduce the search window for loop closures
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 7.
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(30.)
-
--- Lower IMU weights to reduce influence on pose estimation
-POSE_GRAPH.optimization_problem.acceleration_weight = 1e2
-POSE_GRAPH.optimization_problem.rotation_weight = 1e2
+-- turn off logging
+POSE_GRAPH.log_residual_histograms = false
+POSE_GRAPH.constraint_builder.log_matches = false
+POSE_GRAPH.optimization_problem.log_solver_summary = false
 
 return options
