e6989c78fac3d05391575c363e51aa0f3e099f5c
Stephen Dade <stephen_dade@hotmail.com>
2025-12-10 19:18:34 +1100

    ardupilot_cartographer: Performance improvements

--- ardupilot_cartographer/config/cartographer.lua
+++ ardupilot_cartographer/config/cartographer.lua
@@ -18,33 +18,34 @@ options = {
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
-  submap_publish_period_sec = 0.5,
-  pose_publish_period_sec = 10e-3,
-  trajectory_publish_period_sec = 50e-3,
+  submap_publish_period_sec = 1.0,
+  pose_publish_period_sec = 20e-3,
+  trajectory_publish_period_sec = 100e-3,
   rangefinder_sampling_ratio = 1.,
-  odometry_sampling_ratio = 1.,
-  fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 1.,
+  odometry_sampling_ratio = 0.5,
+  fixed_frame_pose_sampling_ratio = 0.5,
+  imu_sampling_ratio = 0.5,
   landmarks_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 MAP_BUILDER.num_background_threads = 1
 
-TRAJECTORY_BUILDER_2D.min_range = 0.27
+TRAJECTORY_BUILDER_2D.min_range = 0.30
 TRAJECTORY_BUILDER_2D.max_range = 30
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 8.5
 TRAJECTORY_BUILDER_2D.use_imu_data = true
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 0.2
 TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 5
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 10
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 5
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.1
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.linear_search_window = 0.08
+TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.angular_search_window = math.rad(15.)
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.translation_delta_cost_weight = 1.
 TRAJECTORY_BUILDER_2D.real_time_correlative_scan_matcher.rotation_delta_cost_weight = 10
-TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.2)
-TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.0
-TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 0.2
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.5)
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.1
+TRAJECTORY_BUILDER_2D.motion_filter.max_time_seconds = 0.5
 -- for current lidar only 1 is good value
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
 
@@ -53,8 +54,20 @@ TRAJECTORY_BUILDER_2D.max_z = 0.5
 
 POSE_GRAPH.constraint_builder.min_score = 0.65
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.65
-POSE_GRAPH.optimization_problem.huber_scale = 1e2
-POSE_GRAPH.optimize_every_n_nodes = 30
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.05
+POSE_GRAPH.constraint_builder.max_constraint_distance = 10.
+POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1.1e4
+POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 1e5
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.linear_search_window = 5.
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.angular_search_window = math.rad(15.)
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher.branch_and_bound_depth = 5
+POSE_GRAPH.constraint_builder.ceres_scan_matcher.occupied_space_weight = 1.
+POSE_GRAPH.constraint_builder.ceres_scan_matcher.translation_weight = 10.
+POSE_GRAPH.constraint_builder.ceres_scan_matcher.rotation_weight = 1.
+POSE_GRAPH.constraint_builder.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 10
+POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 20
+POSE_GRAPH.optimization_problem.ceres_solver_options.num_threads = 1
+POSE_GRAPH.optimize_every_n_nodes = 90
 
 -- Reduce IMU influence on pose estimation
 POSE_GRAPH.optimization_problem.acceleration_weight = 1e1
